0b9e43b1698ec4f6120417b478aedba3749b315c
Vinícius Valle <tardelivinicius@gmail.com>
2024-04-03 15:24:28 +0000

    testando mariadbtest no host

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'TEST': {'NAME': 'test_docker'},
         'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
+        'HOST': 'mariadbtest',
         'PORT': '3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
