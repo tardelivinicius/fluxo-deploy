2bc820e927faa085d4442320a1fe372895abbc7d
Vinícius Valle <tardelivinicius@gmail.com>
2024-04-03 14:57:52 +0000

    corrigido host

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'TEST': {'NAME': 'test_docker'},
         'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': '0.0.0.0',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
