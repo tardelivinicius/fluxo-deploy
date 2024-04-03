0dc8699897b9526c81f70d8d033af32edbefe71c
Vinícius Valle <tardelivinicius@gmail.com>
2024-04-03 15:10:29 +0000

    novas tentativas

--- core/settings.py
+++ core/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': "utf8mb4"
