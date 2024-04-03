b60f81b6c5651464f46a1d9c9b605146212fc686
Vinícius Valle <tardelivinicius@gmail.com>
2024-04-03 14:48:52 +0000

    alterada a pipeline + settings

--- core/settings.py
+++ core/settings.py
@@ -82,11 +82,28 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'CONN_MAX_AGE': 0,
+        'NAME': 'test_example',
+        'TEST': {'NAME': 'test_docker'},
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': "utf8mb4"
+        },
+    },
 }
 
 REST_FRAMEWORK = {
