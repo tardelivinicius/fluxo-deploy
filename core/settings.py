98e062839568fc7758659a52ef7b9f9b296415d9
Vinícius Valle <tardelivinicius@gmail.com>
2024-04-03 15:38:22 -0300

    teste

--- core/settings.py
+++ core/settings.py
@@ -87,10 +87,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
         'TEST': {'NAME': 'test_docker'},
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': "utf8mb4"
-        },
     }
 }
 
